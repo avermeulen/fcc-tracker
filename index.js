e164dc6e18faa8ac096ef87ba8f459a92b2cf756
avermeulen <andre.vermeulen@gmail.com>
2018-07-19 23:22:25 +0200

    added course percentage

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
-
+const jsIntro = require('./data/javascript-intro.json');
+const htmlIntro = require('./data/html5.json');
+const cssIntro = require('./data/css-intro.json');
 const app = express();
-
 const PORT = process.env.PORT || 3001;
+
+function calculateProgress(userActivityList, challangesList){
+    let activities = userActivityList.filter((activity) => challangesList.challenges.includes(activity));
+    let progess = activities.length / challangesList.challenges.length;  
+    return Math.floor(progess * 100);
+}
 
 app.get('/api/progress/:username', async function(req, res, next){
     try {
         const browser = await puppeteer.launch({
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
+        const username = req.params.username;
         const page = await browser.newPage();
-        await page.goto('https://www.freecodecamp.org/' + req.params.username);
+        await page.goto('https://www.freecodecamp.org/' + username);
         await page.waitForSelector('.username', {
             timeout : 5000
         });
@@ -27,20 +35,33 @@ app.get('/api/progress/:username', async function(req, res, next){
                     return (results);
                 
                   }, '.table-striped > tbody > tr > td > a');
-                
-                res.json(userActivity);
+                 
+
+                res.json({
+                    status: 'success',
+                    data: {
+                        username,
+                        js: calculateProgress(userActivity, jsIntro),
+                        html: calculateProgress(userActivity, htmlIntro),
+                        css: calculateProgress(userActivity, cssIntro)
+                    }
+                });
                 await browser.close();
             }
             catch(err) {
-                res.send(err.stack);
+                return json.send({
+                    status: 'error',
+                    error: err.stack
+                });
             }
         }, 1000);
     }
     catch(err){
-        res.send(err.stack);
+        return json.send({
+            status: 'error',
+            error: err.stack
+        });
     }
-
-    
 });
 
 app.listen(PORT, function(){
