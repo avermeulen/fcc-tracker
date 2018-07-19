52f764755ae6b74bb358e48c7de50215f63ee105
avermeulen <andre.vermeulen@gmail.com>
2018-07-19 19:25:09 +0200

    added loggin

--- index.js
+++ index.js
@@ -5,28 +5,38 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/api/progress/:username', async function(req, res){
-    const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
-    const page = await browser.newPage();
-    await page.goto('https://www.freecodecamp.org/' + req.params.username);
-    await page.waitForSelector('.username');
-
-    setTimeout(async function(){
-        let userActivity = await page.evaluate(function(sel){
-            let elems = document.querySelectorAll(sel);
-            let results = [];
-            for (let elem of elems) {
-                results.push(elem.innerHTML);
+app.get('/api/progress/:username', async function(req, res, next){
+    try {
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
+        const page = await browser.newPage();
+        await page.goto('https://www.freecodecamp.org/' + req.params.username);
+        await page.waitForSelector('.username');
+    
+        setTimeout(async function(){
+            try {
+                let userActivity = await page.evaluate(function(sel){
+                    let elems = document.querySelectorAll(sel);
+                    let results = [];
+                    for (let elem of elems) {
+                        results.push(elem.innerHTML);
+                    }
+                    return (results);
+                
+                  }, '.table-striped > tbody > tr > td > a');
+                
+                res.json(userActivity);
+                await browser.close();
             }
-            return (results);
-        
-          }, '.table-striped > tbody > tr > td > a');
-        
-        res.json(userActivity);
-        await browser.close();
-    }, 1000);
+            catch(err) {
+                next(err);
+            }
+        }, 1000);
+    }
+    catch(err){
+        next(err);
+    }
 
     
 });
