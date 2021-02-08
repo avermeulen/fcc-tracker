d96e21ce5fdcc96545a6c9e27c178d5857e92865
avermeulen <andre.vermeulen@gmail.com>
2021-02-08 12:54:33 +0200

    ensure page is closed after use

--- codecamp-progess-scraper.js
+++ codecamp-progess-scraper.js
@@ -6,114 +6,111 @@ const cssIntro = require('./data/css-intro.json');
 function calculateProgress (userActivityList, challangesList) {
 
     let activities = userActivityList.filter((activity) => challangesList.challenges.includes(activity));
-	let progess = activities.length / challangesList.challenges.length;
-	return Math.floor(progess * 100);
-	
+    let progess = activities.length / challangesList.challenges.length;
+    return Math.floor(progess * 100);
 }
 
 module.exports = function (browser) {
-	
-	async function scrape(username) {
-		return new Promise(async function(resolve, reject){
-			try {
 
-				const page = await browser.newPage();
-					await page.goto('https://www.freecodecamp.org/' + username);
-					await page.waitForSelector('.username', {
-						timeout: process.env.PAGE_LOAD_TIMEOUT || 3000
-					});
-		
-				var { userActivity, userPoints, pageCount, lastActiveAt } = await getPageTotals(page);
-				let activities = [...userActivity];
-				
-				const context = {
-					username,
-					js: 0,
-					html: 0,
-					css: 0,
-					userPoints,
-					pageCount,
-					lastActiveAt
-				};
+    async function scrape (username) {
+        return new Promise(async function (resolve, reject) {
+            try {
 
-				for (let currentCount = 1; currentCount < pageCount; currentCount++) {
-					const nextPageButton = "[aria-label='Go to Next page']";
-					await page.click(nextPageButton);
-					var { userActivity } = await getPageTotals(page);
-					// console.log(userActivity.length);
+                const page = await browser.newPage();
+                await page.goto('https://www.freecodecamp.org/' + username);
+                await page.waitForSelector('.username', {
+                    timeout: process.env.PAGE_LOAD_TIMEOUT || 3000
+                });
+                var { userActivity, userPoints, pageCount, lastActiveAt } = await getPageTotals(page);
+                let activities = [...userActivity];	
+                const context = {
+                    username,
+                    js: 0,
+                    html: 0,
+                    css: 0,
+                    userPoints,
+                    pageCount,
+                    lastActiveAt
+                };
 
-					activities = activities.concat(userActivity);
-				}
+                for (let currentCount = 1; currentCount < pageCount; currentCount++) {
+                    const nextPageButton = "[aria-label='Go to Next page']";
+                    await page.click(nextPageButton);
+                    var { userActivity } = await getPageTotals(page);
+                    // console.log(userActivity.length);
 
-				context.js = calculateProgress(activities, jsIntro);
-				context.html = calculateProgress(activities, htmlIntro);
-				context.cssIntro = calculateProgress(activities, cssIntro);
+                    activities = activities.concat(userActivity);
+                }
 
-				resolve({
-					username,
-					js: calculateProgress(activities, jsIntro),
-					html: calculateProgress(activities, htmlIntro),
-					css: calculateProgress(activities, cssIntro),
-					userPoints,
-					pageCount,
-					lastActiveAt
-				});
-		
-			} catch (err) {
-				reject({
-					status: 'error',
-					error: err.stack
-				})
-			}
+                context.js = calculateProgress(activities, jsIntro);
+                context.html = calculateProgress(activities, htmlIntro);
+                context.cssIntro = calculateProgress(activities, cssIntro);
 
-		});
+                resolve({
+                    username,
+                    js: calculateProgress(activities, jsIntro),
+                    html: calculateProgress(activities, htmlIntro),
+                    css: calculateProgress(activities, cssIntro),
+                    userPoints,
+                    pageCount,
+                    lastActiveAt
+                });
+                page.close();
+            } catch (err) {
+                // eslint-disable-next-line prefer-promise-reject-errors
+                reject({
+                    status: 'error',
+                    error: err.stack
+                });
+            }
 
-		async function getPageTotals(page) {
-			let userActivity = await page.evaluate(function (sel) {
-				let elems = document.querySelectorAll(sel);
-				let results = [];
-				for (let elem of elems) {
-					results.push(elem.innerHTML);
-				}
-				return results;
-			}, '.table-striped > tbody > tr > td > a');
+        });
 
-			const userPoints = await page.evaluate(function (sel) {
-				let elem = document.querySelector(sel);
-				return Number(elem.innerText.split(" ")[0]);
-			}, '.text-center.points');
+        async function getPageTotals (page) {
+            let userActivity = await page.evaluate(function (sel) {
+                let elems = document.querySelectorAll(sel);
+                let results = [];
+                for (let elem of elems) {
+                    results.push(elem.innerHTML);
+                }
+                return results;
+            }, '.table-striped > tbody > tr > td > a');
 
-			const pageCount = await page.evaluate(function (sel) {
-				let elems = document.querySelectorAll(sel);
-				if (elems && elems.length > 1) {
-					let parts = elems[1].innerText.split('of');
-					return Number(parts[parts.length - 1]);
-				}
-				return 0;
-			}, '.timeline-pagination_list_item');
+            const userPoints = await page.evaluate(function (sel) {
+                let elem = document.querySelector(sel);
+                return Number(elem.innerText.split(' ')[0]);
+            }, '.text-center.points');
 
-			const lastActiveAt = await page.evaluate(function (sel) {
-				let elem = document.querySelector(sel);
-				if (elem) {
-					return elem.dateTime;
-				}
-				return null;
-			}, 'time');
+            const pageCount = await page.evaluate(function (sel) {
+                let elems = document.querySelectorAll(sel);
+                if (elems && elems.length > 1) {
+                    let parts = elems[1].innerText.split('of');
+                    return Number(parts[parts.length - 1]);
+                }
+                return 0;
+            }, '.timeline-pagination_list_item');
+
+            const lastActiveAt = await page.evaluate(function (sel) {
+                let elem = document.querySelector(sel);
+                if (elem) {
+                    return elem.dateTime;
+                }
+                return null;
+            }, 'time');
 
 
-			await page.screenshot({path: 'page1.png'});
+            await page.screenshot({path: 'page1.png'});
 
-			return { 
-				userActivity, 
-				userPoints, 
-				pageCount, 
-				lastActiveAt 
-			};
-		}
-	}
+            return { 
+                userActivity,
+                userPoints,
+                pageCount,
+                lastActiveAt
+            };
+        }
+    }
 
-	return  {
-		scrape
-	}
-	
-}
+    return {
+        scrape
+    };
+};
